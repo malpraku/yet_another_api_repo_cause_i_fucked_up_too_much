9ad5752877921b9d6605fe523ea157366bb30c1f
I :( PHP <jancok@mail.co.ck>
2023-03-24 09:09:29 +0700

    yg penting jalan dulu main functionny, improvisasi nanti.

--- app/Http/Controllers/API/ProyekController.php
+++ app/Http/Controllers/API/ProyekController.php
@@ -11,23 +11,12 @@ use Illuminate\Http\Resource\ProyekResource;
 
 class ProyekController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $proyek = Proyek::all();
         return response(['proyeks' => ProyekResource::collection($proyeks), 'message' => 'Data berhasil ditampilkan', 200]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $data = $request->all();
@@ -44,36 +33,17 @@ class ProyekController extends Controller
         return response(["proyek" =>new ProyekResource($proyek), "message"=>"Data proyek berhasil ditambahkan."],200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Proyek  $proyek
-     * @return \Illuminate\Http\Response
-     */
     public function show(Proyek $proyek)
     {
         return response(["proyek" =>new ProyekResource($proyek), "message"=>"Data proyek berhasil diambil."],200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Proyek  $proyek
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Proyek $proyek)
     {
         $proyek->update($request->all());
         return response(["proyek" =>new ProyekResource($proyek), "message"=>"Data proyek berhasil diubah."],200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Proyek  $proyek
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Proyek $proyek)
     {
         $proyek->delete();
